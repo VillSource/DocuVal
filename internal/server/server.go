6e4e8f4fa05b3a9a24a0309589342afe8a8b43a5
villsource <30929839+VillSource@users.noreply.github.com>
2025-05-22 01:50:18 +0700

    tmp

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,13 +1,32 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
 
-type DocuvalServer struct {}
+	"github.com/gofiber/fiber/v2"
+	"github.com/villsource/docuval-identity/pkg/identity"
+)
+
+type DocuvalServer struct {
+    fiberApp *fiber.App
+}
 
 func New() *DocuvalServer {
-	return &DocuvalServer{}
+	return &DocuvalServer{
+        fiberApp: fiber.New(),
+    }
 }
 
 func (s *DocuvalServer) Start() {
 	fmt.Println("Docuval server is running...")
+
+    s.configIdentity()
+
+    fmt.Println("http://localhost:3000")
+    s.fiberApp.Listen(":3000")
+}
+
+
+func (s *DocuvalServer) configIdentity() {
+    s.fiberApp.Use(identity.NewFiberMiddleware())
 }
